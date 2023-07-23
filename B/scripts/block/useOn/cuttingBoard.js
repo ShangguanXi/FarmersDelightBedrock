82294548dbbedd0638f4127fe0d5f79bcb0c93c4
EnderDragongugugu <2778871075@qq.com>
2023-07-23 17:37:42 +0800

    完善了小刀的内容

--- B/scripts/block/useOn/cuttingBoard.js
+++ B/scripts/block/useOn/cuttingBoard.js
@@ -1,6 +1,6 @@
 import BlockEntity from "../../lib/BlockEntity";
 import { farmersdelightBlockList, vanillaItemList } from '../../data/recipe/cuttingBoardRecipe'
-import { claerItem } from '../../lib/itemUtil';
+import { claerItem, damageItem } from '../../lib/itemUtil';
 import { MolangVariableMap, ItemStack } from "@minecraft/server";
 
 const molang = new MolangVariableMap();
@@ -17,24 +17,25 @@ export function cuttingBoard(player, itemStack, block) {
     );
     const entity = blockEntity.getEntity();
     const map = blockEntity.getDataMap('item');
-    if (map && itemStack.typeId !== 'farmersdelight:diamond_knife') {
+    if (map && !itemStack.hasTag('farmersdelight:is_knife')) {
         entity.dimension.spawnItem(new ItemStack(map.get('item')), entity.location);
         entity.triggerEvent('farmersdelight:despawn');
         player.dimension.spawnEntity('farmersdelight:cutting_board', location).addTag(JSON.stringify(location));
     }
-    if (farmersdelightBlockList.includes(itemStack.typeId) || vanillaItemList.includes(itemStack.typeId) || itemStack.hasTag('farmersdelight:can_cut') || itemStack.typeId == 'farmersdelight:diamond_knife') {
+    if (farmersdelightBlockList.includes(itemStack.typeId) || vanillaItemList.includes(itemStack.typeId) || itemStack.hasTag('farmersdelight:can_cut') || itemStack.hasTag('farmersdelight:is_knife')) {
         if (map) {
-            if (itemStack.typeId === 'farmersdelight:diamond_knife') {
+            if (itemStack.hasTag('farmersdelight:is_knife')) {
                 const id = map.get('item').split(':')[1];
                 player.runCommandAsync(`loot spawn ${location.x} ${location.y} ${location.z} loot "item/${id}"`);
                 entity.triggerEvent('farmersdelight:despawn');
                 player.dimension.spawnEntity('farmersdelight:cutting_board', location).addTag(JSON.stringify(location));
+                damageItem(container, player.selectedSlot);
             }
-        } else if (itemStack.typeId !== 'farmersdelight:diamond_knife') {
+        } else if (!itemStack.hasTag('farmersdelight:is_knife')) {
             const id = itemStack.typeId.split(':');
             const name = id[0] == 'minecraft' ? `farmersdelight:${id[0]}_${id[1]}` : itemStack.typeId;
             entity.addTag(`{"item":"${itemStack.typeId}"}`);
-            claerItem(itemStack.typeId, container, player.selectedSlot);
+            claerItem( container, player.selectedSlot);
             entity.dimension.spawnParticle(name, { x: location.x + 0.5, y: location.y + 0.07, z: location.z + 0.5 }, molang);
         }
     }
--- B/scripts/lib/itemUtil.js
+++ B/scripts/lib/itemUtil.js
@@ -1,5 +1,5 @@
 
-export function claerItem(id, container, index, amount = 1) {
+export function claerItem( container, index, amount = 1) {
     const itemStack = container.getItem(index);
     const itemAmount = itemStack.amount;
     if (itemAmount > amount) {
@@ -9,5 +9,18 @@ export function claerItem(id, container, index, amount = 1) {
     } else {
         container.setItem(index, undefined);
         return itemAmount;
+    }
+}
+
+export function damageItem( container, index, damage = 1) {
+    const itemStack = container.getItem(index);
+    const durability = itemStack.getComponent("minecraft:durability");
+    if (durability.maxDurability > durability.damage) {
+        durability.damage += damage;
+        container.setItem(index, itemStack);
+        return damage;
+    } else {
+        container.setItem(index, undefined);
+        return durability.maxDurability;
     }
 }
