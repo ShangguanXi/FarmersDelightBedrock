10fd77ef0f286820d19bf3db247f28dd0ef4bfb6
EnderDragongugugu <2778871075@qq.com>
2023-07-26 22:08:27 +0800

    优化了砧板

--- B/scripts/lib/BlockEntity.js
+++ B/scripts/lib/BlockEntity.js
@@ -1,10 +1,54 @@
+import { ItemStack } from "@minecraft/server";
 
+// 同步处理
+export function loot(itemStack, block, entity) {
+    if (itemStack) {
+        if (block !== 'farmersdelight:cutting_board') {
+            entity.dimension.spawnItem(new ItemStack(itemStack), entity.location);
+            entity.triggerEvent('farmersdelight:despawn');
+        }
+    } else if (block !== 'farmersdelight:cutting_board') {
+        entity.triggerEvent('farmersdelight:despawn');
+    }
+}
+
+export function getMap(entity, value) {
+    const map = new Map();
+    for (const tag of entity.getTags()) {
+        if (JSON.parse(tag)) {
+            const json = JSON.parse(tag);
+            map.set(value, json[value]);
+        } else {
+            continue;
+        }
+    }
+    if (map.get(value)) {
+        return map
+    }
+    return undefined;
+}
+
+export function location(entity) {
+    for (const tag of entity.getTags()) {
+        if (JSON.parse(tag)) {
+            const json = JSON.parse(tag);
+            if (json.x) {
+                return json
+            }
+        } else {
+            continue;
+        }
+    }
+    return undefined;
+}
+// class
 export default class BlockEntity {
     id;
     options;
     dimension;
     entity;
     map = new Map();
+    #tags;
     constructor(id, dimension, options) {
         this.id = id;
         this.dimension = dimension;
@@ -16,17 +60,20 @@ export default class BlockEntity {
                 break;
             }
         }
+        if (this.entity) {
+            this.#tags = this.entity.getTags();
+        }
     }
     getEntity() {
         return this.entity;
     }
     getDataMap(value) {
         if (this.entity) {
-            for (const tag of this.entity.getTags()) {
+            for (const tag of this.#tags) {
                 if (JSON.parse(tag)) {
                     const json = JSON.parse(tag);
                     this.map.set(value, json[value]);
-                }else{
+                } else {
                     continue;
                 }
             }
@@ -36,12 +83,17 @@ export default class BlockEntity {
         }
         return undefined;
     }
-    loot() {
-        for (const [key, value] of map) {
-            if (value.id) {
-                const item = new ItemStack(value.id);
-                this.dimension.spawnItem(item, this.entity.location);
+    blockLocation(entity) {
+        for (const tag of entity.getTags()) {
+            if (JSON.parse(tag)) {
+                const json = JSON.parse(tag);
+                if (json.x) {
+                    return json
+                }
+            } else {
+                continue;
             }
         }
+        return undefined;
     }
 }
