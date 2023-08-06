7b58761b963550ed24d059db1302800690dd7947
诗栩MSolo <65381286+xMarioHux@users.noreply.github.com>
2023-08-06 22:08:43 +0800

    沃土耕地初步

---
+++ B/scripts/block/randomTick/richSoilFarmland.js
@@ -0,0 +1,7 @@
+
+
+export function richSoilFarmland(args) {
+    const farmlandBlock = args.sourceBlock;
+    const location = args.sourceBlock.location;
+    const dimension = args.sourceBlock.dimension;
+}
