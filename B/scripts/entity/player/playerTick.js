add8254493725ba92b8847d37c2f97bc3f4163d3
上官汐 <78731793+ShangguanXi@users.noreply.github.com>
2023-08-15 18:59:38 +0800

    小更改

--- B/scripts/entity/player/playerTick.js
+++ B/scripts/entity/player/playerTick.js
@@ -2,15 +2,16 @@ import { world, system } from "@minecraft/server";
 
 function rope(player) {
     const rope = player.dimension.getBlock(player.location).typeId === 'farmersdelight:rope_block';
+    const RX = player.getRotation().x;
     const RY = player.getRotation().y;
     if (rope) {
         // player.addEffect('levitation', 2, { amplifier: 10, showParticles: false });
         // player.applyImpulse({ x: location.x, y: location.y += 1, z: location.z });
         if (RY <= -20) {
-            player.addEffect('levitation', 2, { amplifier: 10, showParticles: false });
+            player.addEffect('levitation', 1, { amplifier: 10, showParticles: false });
         }
         if (RY >= 20) {
-            player.addEffect('slow_falling', 2, { amplifier: 10, showParticles: false });
+            player.addEffect('slow_falling', 1, { amplifier: 10, showParticles: false });
         }
 
     }
