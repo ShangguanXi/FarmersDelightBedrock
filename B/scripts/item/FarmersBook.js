e27727f31b957ec90ff8c5ab23ab6283609a9ff0
上官汐 <78731793+ShangguanXi@users.noreply.github.com>
2024-02-21 21:01:03 +0800

    表单

--- B/scripts/item/FarmersBook.js
+++ B/scripts/item/FarmersBook.js
@@ -10,32 +10,90 @@ var __metadata = (this && this.__metadata) || function (k, v) {
 import { ItemUseAfterEvent, world } from "@minecraft/server";
 import { methodEventSub } from "../lib/eventHelper";
 import { ActionFormData } from '@minecraft/server-ui';
+function thanksForm(player) {
+    const thanks = new ActionFormData()
+        .title({ "rawtext": [{ "text": "farmersdelight.book.thanks" }] })
+        .body({
+        "rawtext": [
+            { "translate": "farmersdelight.book.thanks.sponsored_list_title" },
+            { "text": "\n" },
+            { "translate": "farmersdelight.book.thanks.sponsored_list" },
+            { "text": "\n" },
+            { "translate": "farmersdelight.book.thanks.license_title" },
+            { "text": "\n" },
+            { "translate": "farmersdelight.book.thanks.license" },
+            { "text": "\n" },
+            { "translate": "farmersdelight.book.thanks.developers_title" },
+            { "text": "\n" },
+            { "translate": "farmersdelight.book.thanks.developers" },
+            { "text": "\n" },
+            { "translate": "farmersdelight.book.thanks.contributors_title" },
+            { "text": "\n" },
+            { "translate": "farmersdelight.book.thanks.contributors" }
+        ]
+    })
+        .button({ "rawtext": [{ "text": "farmersdelight.book.back" }] });
+    thanks.show(player).then((response) => {
+        if (response.selection === 0) {
+            mainForm(player);
+        }
+    });
+}
+;
+function cuttingBoardForm(player) {
+    const thanks = new ActionFormData()
+        .title({ "rawtext": [{ "text": "tile.farmersdelight:cutting_board.name" }] })
+        .body({
+        "rawtext": [
+            { "translate": "farmersdelight.book.thanks.sponsored_list_title" },
+            { "text": "\n" },
+            { "translate": "farmersdelight.book.thanks.sponsored_list" },
+            { "text": "\n" },
+            { "translate": "farmersdelight.book.thanks.license_title" },
+            { "text": "\n" },
+            { "translate": "farmersdelight.book.thanks.license" },
+            { "text": "\n" },
+            { "translate": "farmersdelight.book.thanks.developers_title" },
+            { "text": "\n" },
+            { "translate": "farmersdelight.book.thanks.developers" },
+            { "text": "\n" },
+            { "translate": "farmersdelight.book.thanks.contributors_title" },
+            { "text": "\n" },
+            { "translate": "farmersdelight.book.thanks.contributors" }
+        ]
+    })
+        .button({ "rawtext": [{ "text": "farmersdelight.book.back" }] });
+    thanks.show(player).then((response) => {
+        if (response.selection === 0) {
+            mainForm(player);
+        }
+    });
+}
+;
+function mainForm(player) {
+    const form = new ActionFormData()
+        .title({ "rawtext": [{ "text": "farmersdelight.book.title" }] })
+        .button({ "rawtext": [{ "text": "tile.farmersdelight:cutting_board.name" }] })
+        .button({ "rawtext": [{ "text": "tile.farmersdelight:stove.name" }] })
+        .button({ "rawtext": [{ "text": "tile.farmersdelight:cooking_pot.name" }] })
+        .button({ "rawtext": [{ "text": "tile.farmersdelight:skillet_block.name" }] })
+        .button({ "rawtext": [{ "text": "farmersdelight.book.crop" }] })
+        .button({ "rawtext": [{ "text": "farmersdelight.book.thanks" }] });
+    form.show(player).then((response) => {
+        if (response.selection === 5) {
+            thanksForm(player);
+        }
+    });
+}
+;
 export class FarmersBook {
     itemUse(args) {
         const player = args.source;
         const itemStack = args.itemStack;
         if (itemStack?.typeId == "farmersdelight:book_farmersdelight") {
-            const form = new ActionFormData()
-                .title({ "rawtext": [{ "text": "farmersdelight.book.title" }] })
-                .button({ "rawtext": [{ "text": "tile.farmersdelight:cutting_board.name" }] })
-                .button({ "rawtext": [{ "text": "tile.farmersdelight:stove.name" }] })
-                .button({ "rawtext": [{ "text": "tile.farmersdelight:cooking_pot.name" }] })
-                .button({ "rawtext": [{ "text": "tile.farmersdelight:skillet_block.name" }] })
-                .button({ "rawtext": [{ "text": "farmersdelight.book.crop" }] })
-                .button({ "rawtext": [{ "text": "farmersdelight.book.thanks" }] });
-            form.show(player).then((response) => {
-                if (response.selection === 5) {
-                    const thanks = new ActionFormData()
-                        .title({ "rawtext": [{ "text": "farmersdelight.book.thanks" }] })
-                        .body({ "rawtext": [{ "text": "farmersdelight.book.thanks.body" }] })
-                        .button({ "rawtext": [{ "text": "farmersdelight.book.back" }] });
-                    thanks.show(player).then((response) => {
-                    });
-                }
-            });
+            mainForm(player);
         }
     }
-    ;
 }
 __decorate([
     methodEventSub(world.afterEvents.itemUse),
@@ -43,4 +101,5 @@ __decorate([
     __metadata("design:paramtypes", [ItemUseAfterEvent]),
     __metadata("design:returntype", void 0)
 ], FarmersBook.prototype, "itemUse", null);
+;
 //# sourceMappingURL=FarmersBook.js.map
--- B/typescripts/item/FarmersBook.ts
+++ B/typescripts/item/FarmersBook.ts
@@ -2,37 +2,96 @@ import { ItemStack, Player, ItemUseAfterEvent, world } from "@minecraft/server";
 import { methodEventSub } from "../lib/eventHelper";
 import { ActionFormData, ActionFormResponse } from '@minecraft/server-ui';
 
+function thanksForm(player: Player) {
+    const thanks = new ActionFormData()
+        .title({ "rawtext": [{ "text": "farmersdelight.book.thanks" }] })
+        .body({
+            "rawtext": [
+                { "translate": "farmersdelight.book.thanks.sponsored_list_title" },
+                { "text": "\n" },
+                { "translate": "farmersdelight.book.thanks.sponsored_list" },
+                { "text": "\n" },
+                { "translate": "farmersdelight.book.thanks.license_title" },
+                { "text": "\n" },
+                { "translate": "farmersdelight.book.thanks.license" },
+                { "text": "\n" },
+                { "translate": "farmersdelight.book.thanks.developers_title" },
+                { "text": "\n" },
+                { "translate": "farmersdelight.book.thanks.developers" },
+                { "text": "\n" },
+                { "translate": "farmersdelight.book.thanks.contributors_title" },
+                { "text": "\n" },
+                { "translate": "farmersdelight.book.thanks.contributors" }
+            ]
+        }
+        )
+        .button({ "rawtext": [{ "text": "farmersdelight.book.back" }] });
+
+    thanks.show(player).then((response: ActionFormResponse) => {
+        if (response.selection === 0) {
+            mainForm(player);
+        }
+    })
+};
+function cuttingBoardForm(player: Player) {
+    const thanks = new ActionFormData()
+        .title({ "rawtext": [{ "text": "tile.farmersdelight:cutting_board.name" }] })
+        .body({
+            "rawtext": [
+                { "translate": "farmersdelight.book.thanks.sponsored_list_title" },
+                { "text": "\n" },
+                { "translate": "farmersdelight.book.thanks.sponsored_list" },
+                { "text": "\n" },
+                { "translate": "farmersdelight.book.thanks.license_title" },
+                { "text": "\n" },
+                { "translate": "farmersdelight.book.thanks.license" },
+                { "text": "\n" },
+                { "translate": "farmersdelight.book.thanks.developers_title" },
+                { "text": "\n" },
+                { "translate": "farmersdelight.book.thanks.developers" },
+                { "text": "\n" },
+                { "translate": "farmersdelight.book.thanks.contributors_title" },
+                { "text": "\n" },
+                { "translate": "farmersdelight.book.thanks.contributors" }
+            ]
+        }
+        )
+        .button({ "rawtext": [{ "text": "farmersdelight.book.back" }] });
+
+    thanks.show(player).then((response: ActionFormResponse) => {
+        if (response.selection === 0) {
+            mainForm(player);
+        }
+    })
+};
+function mainForm(player: Player) {
+    const form = new ActionFormData()
+        .title({ "rawtext": [{ "text": "farmersdelight.book.title" }] })
+        .button({ "rawtext": [{ "text": "tile.farmersdelight:cutting_board.name" }] })
+        .button({ "rawtext": [{ "text": "tile.farmersdelight:stove.name" }] })
+        .button({ "rawtext": [{ "text": "tile.farmersdelight:cooking_pot.name" }] })
+        .button({ "rawtext": [{ "text": "tile.farmersdelight:skillet_block.name" }] })
+        .button({ "rawtext": [{ "text": "farmersdelight.book.crop" }] })
+        .button({ "rawtext": [{ "text": "farmersdelight.book.thanks" }] });
+
+    form.show(player).then((response: ActionFormResponse) => {
+        if (response.selection === 5) {
+            thanksForm(player);
+        }
+
+    })
+};
 export class FarmersBook {
+
 
     @methodEventSub(world.afterEvents.itemUse)
     itemUse(args: ItemUseAfterEvent) {
         const player: Player = args.source;
         const itemStack: ItemStack | undefined = args.itemStack;
         if (itemStack?.typeId == "farmersdelight:book_farmersdelight") {
-            const form = new ActionFormData()
-                .title({ "rawtext": [{ "text": "farmersdelight.book.title" }] })
-                .button({ "rawtext": [{ "text": "tile.farmersdelight:cutting_board.name" }] })
-                .button({ "rawtext": [{ "text": "tile.farmersdelight:stove.name" }] })
-                .button({ "rawtext": [{ "text": "tile.farmersdelight:cooking_pot.name" }] })
-                .button({ "rawtext": [{ "text": "tile.farmersdelight:skillet_block.name" }] })
-                .button({ "rawtext": [{ "text": "farmersdelight.book.crop" }] })
-                .button({ "rawtext": [{ "text": "farmersdelight.book.thanks" }] });
-
-            form.show(player).then((response: ActionFormResponse) => {
-                if (response.selection === 5) {
-                    const thanks = new ActionFormData()
-                        .title({ "rawtext": [{ "text": "farmersdelight.book.thanks" }] })
-                        .body({ "rawtext": [{ "text": "farmersdelight.book.thanks.body" }] })
-                        .button({ "rawtext": [{ "text": "farmersdelight.book.back" }] });
-
-                    thanks.show(player).then((response: ActionFormResponse) => {
-
-                    })
-
-                }
-            });
+            mainForm(player);
         }
-    };
+    }
+};
 
 
-}
