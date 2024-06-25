5f2ebfc2457eac6862bc2e23e100a97e605c8b94
上官汐 <78731793+ShangguanXi@users.noreply.github.com>
2024-06-25 18:26:30 +0800

    refactor：除稻米之外的作物脱离HCF

--- B/scripts/customComponents/block/CropComponent.js
+++ B/scripts/customComponents/block/CropComponent.js
@@ -7,7 +7,7 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
-import { WorldInitializeBeforeEvent, world, EntityInventoryComponent } from "@minecraft/server";
+import { WorldInitializeBeforeEvent, world, EntityInventoryComponent, Direction } from "@minecraft/server";
 import { methodEventSub } from "../../lib/eventHelper";
 import { ItemUtil } from "../../lib/ItemUtil";
 function spawnLoot(path, dimenion, location) {
@@ -29,23 +29,29 @@ class CropsComponent {
             return;
         const container = player.getComponent(EntityInventoryComponent.componentId)?.container;
         const lootTable = this.getLootTable();
-        if (itemId == "minecraft:bone_meal" && age < 7) {
-            if (player?.getGameMode() == "creative") {
-                block.setPermutation(block.permutation.withState("farmersdelight:growth", 7));
-            }
-            else {
-                if (random <= 60) {
-                    block.setPermutation(block.permutation.withState("farmersdelight:growth", age + 1));
+        try {
+            if (itemId == "minecraft:bone_meal" && age < 7) {
+                world.playSound("item.bone_meal.use", block.location);
+                if (player?.getGameMode() == "creative") {
+                    block.dimension.spawnParticle("minecraft:crop_growth_emitter", { x: block.location.x + 0.5, y: block.location.y + 0.5, z: block.location.z + 0.5 });
+                    block.setPermutation(block.permutation.withState("farmersdelight:growth", 7));
                 }
-                block.dimension.spawnParticle("minecraft:crop_growth_emitter", { x: block.location.x + 0.5, y: block.location.y + 0.5, z: block.location.z + 0.5 });
-                if (!container)
-                    return;
-                ItemUtil.clearItem(container, player?.selectedSlotIndex);
+                else {
+                    if (random <= 60) {
+                        block.setPermutation(block.permutation.withState("farmersdelight:growth", age + 1));
+                    }
+                    block.dimension.spawnParticle("minecraft:crop_growth_emitter", { x: block.location.x + 0.5, y: block.location.y + 0.5, z: block.location.z + 0.5 });
+                    if (!container)
+                        return;
+                    ItemUtil.clearItem(container, player?.selectedSlotIndex);
+                }
+            }
+            if (age == 7) {
+                block.setPermutation(block.permutation.withState("farmersdelight:growth", 0));
+                spawnLoot(lootTable, dimension, { x: block.location.x, y: block.location.y, z: block.location.z });
             }
         }
-        if (age == 7) {
-            block.setPermutation(block.permutation.withState("farmersdelight:growth", 0));
-            spawnLoot(lootTable, dimension, { x: block.location.x, y: block.location.y, z: block.location.z });
+        catch (error) {
         }
     }
     onRandomTick(args) {
@@ -74,11 +80,175 @@ class TomatoComponent extends CropsComponent {
         return "farmersdelight/crops/farmersdelight_tomato_riped";
     }
 }
+class Beetrootomponent extends CropsComponent {
+    getLootTable() {
+        return "farmersdelight/crops/beetroot_riped";
+    }
+}
+class CarrotComponent extends CropsComponent {
+    getLootTable() {
+        return "farmersdelight/crops/carrot_riped";
+    }
+}
+class PotatoComponent extends CropsComponent {
+    getLootTable() {
+        return "farmersdelight/crops/potato_riped";
+    }
+}
+class WheatComponent extends CropsComponent {
+    getLootTable() {
+        return "farmersdelight/crops/wheat_riped";
+    }
+}
+class TorchflowerComponent {
+    constructor() {
+        this.onPlayerInteract = this.onPlayerInteract.bind(this);
+        this.onRandomTick = this.onRandomTick.bind(this);
+    }
+    onPlayerInteract(args) {
+        const block = args.block;
+        const player = args.player;
+        const dimension = args.dimension;
+        const itemId = player?.getComponent("inventory")?.container?.getSlot(player.selectedSlotIndex).typeId;
+        const age = Number(block.permutation.getState("farmersdelight:growth"));
+        const random = Math.floor(Math.random() * 101);
+        if (!player)
+            return;
+        const container = player.getComponent(EntityInventoryComponent.componentId)?.container;
+        try {
+            if (itemId == "minecraft:bone_meal" && age < 7) {
+                if (player?.getGameMode() == "creative") {
+                    block.dimension.spawnParticle("minecraft:crop_growth_emitter", { x: block.location.x + 0.5, y: block.location.y + 0.5, z: block.location.z + 0.5 });
+                    block.setPermutation(block.permutation.withState("farmersdelight:growth", 7));
+                }
+                else {
+                    if (random <= 60) {
+                        block.setPermutation(block.permutation.withState("farmersdelight:growth", age + 1));
+                    }
+                    block.dimension.spawnParticle("minecraft:crop_growth_emitter", { x: block.location.x + 0.5, y: block.location.y + 0.5, z: block.location.z + 0.5 });
+                    if (!container)
+                        return;
+                    ItemUtil.clearItem(container, player?.selectedSlotIndex);
+                }
+                world.playSound("item.bone_meal.use", block.location);
+            }
+            if (itemId == "minecraft:bone_meal" && age == 7) {
+                world.playSound("item.bone_meal.use", block.location);
+                block.dimension.spawnParticle("minecraft:crop_growth_emitter", { x: block.location.x + 0.5, y: block.location.y + 0.5, z: block.location.z + 0.5 });
+                dimension.setBlockType(block.location, "farmersdelight:rich_soil_torchflower");
+            }
+        }
+        catch (error) {
+        }
+    }
+    onRandomTick(args) {
+        const block = args.block;
+        const dimension = args.dimension;
+        const age = Number(block.permutation.getState("farmersdelight:growth"));
+        if (age < 7) {
+            block.setPermutation(block.permutation.withState("farmersdelight:growth", age + 1));
+        }
+        else {
+            dimension.setBlockType(block.location, "farmersdelight:rich_soil_torchflower");
+        }
+    }
+}
+class SugarCaneComponent {
+    constructor() {
+        this.onPlayerInteract = this.onPlayerInteract.bind(this);
+        this.onRandomTick = this.onRandomTick.bind(this);
+    }
+    onPlayerInteract(args) {
+        const block = args.block;
+        const face = args.face;
+        const player = args.player;
+        const dimension = args.dimension;
+        const itemId = player?.getComponent("inventory")?.container?.getSlot(player.selectedSlotIndex).typeId;
+        const age = Number(block.permutation.getState("farmersdelight:growth"));
+        const random = Math.floor(Math.random() * 101);
+        const topLocation = { x: block.location.x, y: block.location.y + 1, z: block.location.z };
+        if (!player)
+            return;
+        const container = player.getComponent(EntityInventoryComponent.componentId)?.container;
+        try {
+            if (itemId == "minecraft:sugar_cane") {
+                if (face != Direction.Up)
+                    return;
+                if (block.typeId == "farmersdelight:rich_soil_sugar_cane_bottom") {
+                    world.playSound("dig.grass", block.location);
+                    dimension.setBlockType(topLocation, "farmersdelight:rich_soil_sugar_cane_middle");
+                }
+                ;
+                if (block.typeId == "farmersdelight:rich_soil_sugar_cane_middle") {
+                    world.playSound("dig.grass", block.location);
+                    dimension.setBlockType(topLocation, "farmersdelight:rich_soil_sugar_cane_top");
+                }
+                ;
+                if (block.typeId == "farmersdelight:rich_soil_sugar_cane_top") {
+                    world.playSound("dig.grass", block.location);
+                    dimension.setBlockType(topLocation, "farmersdelight:rich_soil_sugar_cane_top");
+                }
+                ;
+            }
+            if (itemId == "minecraft:bone_meal") {
+                if (block.typeId == "farmersdelight:rich_soil_sugar_cane_bottom") {
+                    if (dimension.getBlock(topLocation)?.typeId == "minecraft:air") {
+                        if (dimension.getBlock({ x: block.location.x, y: block.location.y + 2, z: block.location.z })?.typeId == "minecraft:air") {
+                            dimension.setBlockType({ x: block.location.x, y: block.location.y + 2, z: block.location.z }, "farmersdelight:rich_soil_sugar_cane_top");
+                        }
+                        dimension.setBlockType(topLocation, "farmersdelight:rich_soil_sugar_cane_middle");
+                        block.dimension.spawnParticle("minecraft:crop_growth_emitter", { x: block.location.x + 0.5, y: block.location.y + 0.5, z: block.location.z + 0.5 });
+                        world.playSound("item.bone_meal.use", block.location);
+                        if (!container)
+                            return;
+                        ItemUtil.clearItem(container, player?.selectedSlotIndex);
+                    }
+                }
+                if (block.typeId == "farmersdelight:rich_soil_sugar_cane_middle") {
+                    if (dimension.getBlock(topLocation)?.typeId == "minecraft:air") {
+                        dimension.setBlockType(topLocation, "farmersdelight:rich_soil_sugar_cane_top");
+                        block.dimension.spawnParticle("minecraft:crop_growth_emitter", { x: block.location.x + 0.5, y: block.location.y + 0.5, z: block.location.z + 0.5 });
+                        world.playSound("item.bone_meal.use", block.location);
+                        if (!container)
+                            return;
+                        ItemUtil.clearItem(container, player?.selectedSlotIndex);
+                    }
+                }
+            }
+        }
+        catch (error) {
+        }
+    }
+    onRandomTick(args) {
+        const block = args.block;
+        const dimension = args.dimension;
+        const age = Number(block.permutation.getState("farmersdelight:growth"));
+        const topLocation = { x: block.location.x, y: block.location.y + 1, z: block.location.z };
+        if (age < 15) {
+            block.setPermutation(block.permutation.withState("farmersdelight:growth", age + 1));
+        }
+        else {
+            if (dimension.getBlock(topLocation)?.typeId == "minecraft:air" && block.typeId == "farmersdelight:rich_soil_sugar_cane_bottom") {
+                dimension.setBlockType(topLocation, "farmersdelight:rich_soil_sugar_cane_middle");
+            }
+            ;
+            if (dimension.getBlock(topLocation)?.typeId == "minecraft:air" && block.typeId == "farmersdelight:rich_soil_sugar_cane_middle") {
+                dimension.setBlockType(topLocation, "farmersdelight:rich_soil_sugar_cane_top");
+            }
+        }
+    }
+}
 export class CropComponentRegister {
     register(args) {
         args.blockTypeRegistry.registerCustomComponent('farmersdelight:cabbage', new CabbageComponent());
         args.blockTypeRegistry.registerCustomComponent('farmersdelight:onion', new OnionComponent());
         args.blockTypeRegistry.registerCustomComponent('farmersdelight:tomato', new TomatoComponent());
+        args.blockTypeRegistry.registerCustomComponent('farmersdelight:beetroot', new Beetrootomponent());
+        args.blockTypeRegistry.registerCustomComponent('farmersdelight:carrot', new CarrotComponent());
+        args.blockTypeRegistry.registerCustomComponent('farmersdelight:potato', new PotatoComponent());
+        args.blockTypeRegistry.registerCustomComponent('farmersdelight:wheat', new WheatComponent());
+        args.blockTypeRegistry.registerCustomComponent('farmersdelight:torchflower', new TorchflowerComponent());
+        args.blockTypeRegistry.registerCustomComponent('farmersdelight:sugar_cane', new SugarCaneComponent());
     }
 }
 __decorate([
