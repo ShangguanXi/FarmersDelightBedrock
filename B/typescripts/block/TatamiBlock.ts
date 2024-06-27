84cd7c2af82b6bdd2d7d12196b94a68fee028911
上官汐 <78731793+ShangguanXi@users.noreply.github.com>
2024-06-27 17:34:21 +0800

    refactor：删除方块行为里的event

---
+++ B/typescripts/block/TatamiBlock.ts
@@ -0,0 +1,56 @@
+import { Block, BlockPermutation, Dimension, Direction, ItemStack, PlayerPlaceBlockBeforeEvent, Vector3, system, world } from "@minecraft/server";
+import { methodEventSub } from "../lib/eventHelper";
+
+export class TatamiBlock {
+    @methodEventSub(world.beforeEvents.playerPlaceBlock)
+    placeBlock(args: PlayerPlaceBlockBeforeEvent) {
+        const block: Block = args.block;
+        if (block.typeId != "farmersdelight:tatami" )return;
+        let { x, y, z }: Vector3 = block.location;
+        const dimension: Dimension = block.dimension;
+        const face: Direction = args.face;
+        system.run(() => {
+            const blockPermutation: BlockPermutation = BlockPermutation.resolve('farmersdelight:tatami');
+            const permutation: BlockPermutation = block.permutation;
+            let thisPerm: string = 'south';
+            let neighborPerm: string = 'north';
+            switch (face) {
+                case Direction.North:
+                    thisPerm = 'south';
+                    neighborPerm = 'north';
+                    z -= 1;
+                    break;
+                case Direction.South:
+                    thisPerm = 'north';
+                    neighborPerm = 'south';
+                    z += 1;
+                    break;
+                case Direction.East:
+                    thisPerm = 'west';
+                    neighborPerm = 'east';
+                    x += 1;
+                    break;
+                case Direction.West:
+                    thisPerm = 'east';
+                    neighborPerm = 'west';
+                    x -= 1;
+                    break;
+                case Direction.Up:
+                    thisPerm = 'down';
+                    neighborPerm = 'up';
+                    y += 1;
+                    break;
+                case Direction.Down:
+                    thisPerm = 'up';
+                    neighborPerm = 'down';
+                    y -= 1;
+                    break;
+                default:
+                    break;
+            };
+            if (dimension.getEntitiesAtBlockLocation({ x, y, z }).length != 0 && dimension.getBlock({ x, y, z })?.typeId != 'farmersdelight:tatami') return;
+            block.setPermutation(permutation.withState('farmersdelight:connection', neighborPerm));
+            dimension.getBlock({ x, y, z })?.setPermutation(blockPermutation.withState("farmersdelight:connection", thisPerm));
+        })
+    }
+}
